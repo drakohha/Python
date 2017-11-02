228f889a4efda3b1da8a0c93a424f2718a0b14c4
drakohha <drakohha@tut.by>
2017-11-02 09:06:10 +0300

    new

--- Test/tamogochi.py
+++ Test/tamogochi.py
@@ -13,7 +13,7 @@ def fun_proverki(i,j):  #Проверка на правельность ввод
             print("Неверный ввод , введите в диапозоне от ",i," до ",j)
     return n
 
-class Critter():
+class Critter():   #класс питомца
     rabota=0
     sutost=5
     nastroenie=3
@@ -22,18 +22,32 @@ class Critter():
         self.name=name
     def __str__(self):
         return self.name
-    def kormlenie(self,x):
+    def kormlenie(self,x):  #функция кормления
         self.sutost+=x
-    def radost(self,x):
+    def radost(self,x):   #функция поднятия настроения
         self.nastroenie+=x
-    def pahat(self,x):
-        self.rabota+=x
-    def __call__(self):
+    def pahat(self,x):   #функция работы питомца от настроения
+        if self.nastroenie<=10:
+            self.rabota+=x-3
+            self.kormlenie(-6)
+            self.radost(-1)
+        elif self.nastroenie >10:
+            self.rabota+=x
+            self.kormlenie(-3)
+            self.radost(-1)
+            
+            
+    def __call__(self):   #функция будет отвечать за состояние питомца
         #print(self.name)
         if self.sutost<=10 and self.sutost>0:
             print("Животинка очень голодная срочно покормите!!")
         elif self.sutost<=0:
             print("Животинка умерла от голода!!")
+            self.flag_hinsi=0
+        if self.nastroenie<=10 and self.nastroenie>0:
+            print("Животинка не в настроении... работать будет плохо !")
+        elif self.nastroenie<=0:
+            print("Животнка была в сильной дипресии и самоубилась ")
             self.flag_hinsi=0
 
 
@@ -42,20 +56,21 @@ class Critter():
 
 
 
-def fun_vzaim(criter,tek):
+def fun_vzaim(criter,tek):   #функция игры (создание выбор животинки)
     flag_de=10
     while True:
+        system('clr') 
         print("Меню взаимодействия : 1-кормить 2- поднять настроение 3-послать животинку работать 0-выйти")
+        print("0- выход")
         print("Текущий результат : ")
         print("Имя животинки = ",criter[tek].name)
         print("Уровень сытости = ",criter[tek].sutost)
-        criter[tek]()
         print("Уровень настроения = ",criter[tek].nastroenie)
-        if criter[tek].nastroenie<=10 and criter[tek].nastroenie>0:
-            print("Животинка не в настроении... работать будет плохо !")
-        elif criter[tek].nastroenie<=0:
-            print("Животнка была в сильной дипресии и самоубилась ")
+        print("Сделаная работа = ",criter[tek].rabota ,"%")
+        criter[tek]()
+        if criter[tek].flag_hinsi==0:
             print("Конец игры!")
+            #criter.pop(tek)
             break
         
         flag_de=fun_proverki(0,3)
@@ -66,21 +81,24 @@ def fun_vzaim(criter,tek):
         elif flag_de==0:
             break
         elif flag_de==3:
-            criter[tek].kormlenie(-4)
-            criter[tek].radost(-1)
             criter[tek].pahat(5)
-        if criter[tek].rabota>=30:
+        if criter[tek].rabota>=100:
             print("Животинка закончила за вас всю работу!!!")
             print("Вы победили!!")
             break
 
-def fun_igra():
+def fun_igra():  #функция работы с питомцем 
     print("Вы начали новую игру!!")
     index=0
     flag_menu=10
     global criter
     criter=[]
-    while True:
+    while True:   #проверка на наличие сдохшей животинки
+        if index>0:
+            if criter[index-1].flag_hinsi==0:
+                criter.pop(index-1)
+                index-=1
+        system('clr')        
         print("Выберете действие : ")
         print("1- Родить новую животного")
         print("2-Выбрать текущию животного")
@@ -95,8 +113,12 @@ def fun_igra():
             break
         elif flag_menu==2:
             print("Выберите одного из доступных животных : (0-4)")
-            print("доступны :" , criter[0].name)
-            tek=fun_proverki(0,4)
+            for i in range(0,index):
+                print("доступны :" , criter[i].name, " под номером : ",i)
+            if index >0:
+                tek=fun_proverki(0,index)
+            else :
+                print("Нету созданых животинок")
             
         elif flag_menu==3:
             print("Модуль взаимодействия с выбраной животинкой")
